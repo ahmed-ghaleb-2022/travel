ae2962e72297590ec8bc90a5894a6980db0fe5fb
ahmed-ghaleb-2022 <ahmedtareq19932022@gmail.com>
2022-04-29 11:07:30 +0200

    first commit

---
+++ app/Models/housingtype.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class housingtype extends Model
+{
+    use HasFactory;
+/*
+    public function housinginfos (){
+
+        return $this->hasMany(housinginfo::class);
+
+    }  */
+}
